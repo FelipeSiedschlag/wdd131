532479fe02df1a8273387c5396b067c6822ddc88
FelipeSiedschlag <fsiedsch@byupathway.edu>
2025-03-22 19:45:04 -0300

    Finish week 03 Places assignment

--- scripts/place.js
+++ scripts/place.js
@@ -17,3 +17,34 @@ lastmodified.innerHTML = `Last modification: <span class="highlight">${new Intl.
         // timeZone: "Brasilia,Brazil",
 	}
 ).format(lastModifiedDate)}</span>`;
+
+
+
+// 
+function calculateWindChill(temperature, windSpeed) {
+    return (temperature <= 10 && windSpeed > 4.8) ? 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16)) : "N/A";
+}
+
+
+// Get the elements from the DOM
+const temperatureInput = document.querySelector('#temperature');
+const windSpeedInput = document.querySelector('#wind');
+const conditionInput = document.querySelector('#condition');
+
+// Set the input values
+temperatureInput.value = "9°C";
+windSpeedInput.value = "10km/h";
+conditionInput.value = "Partly Cloudy";
+
+// Extract the numeric values from the input strings (removing non-numeric characters)
+let temperatureValue = parseFloat(temperatureInput.value); // Converts "9°C" to 9
+let windSpeedValue = parseFloat(windSpeedInput.value); // Converts "10km/h" to 10
+
+windChill = calculateWindChill(temperatureValue, windSpeedValue);
+
+if (windChill !== "N/A") {
+    windChill = windChill.toFixed(1); // Formats the number to 2 decimal places
+}
+document.querySelector('#windChill').value = `${windChill}°C`;
+
+
